606f19c999fcba1611557cd227d8073e9ab1f75b
Yuriy <prezlyata.yuriy@gmail.com>
2017-10-13 19:16:30 +0300

    render true list

--- public/js/common.js
+++ public/js/common.js
@@ -1,154 +1,97 @@
-function getJSONFile(callback) {
-    var httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = function() {
-        if (httpRequest.readyState === 4) {
-            if (httpRequest.status === 200) {
-                var data = JSON.parse(httpRequest.responseText);
-                if (callback) callback(data);
-            }
-        }
-    };
-    httpRequest.open('GET', '/structure-source', true);
-    httpRequest.send();
-}
-
-var container = document.createElement('div');
-container.className = 'container';
-document.body.appendChild(container);
-
-var list = document.createElement('ul');
-list.id = 'items';
-container.appendChild(list);
-
-
-getJSONFile(function(data) {
-
-    var list = data.tree;
-
-    function treeify(list, idAttr, parentAttr, childrenAttr) {
-
-        if (!idAttr) idAttr = 'id';
-        if (!parentAttr) parentAttr = 'parent';
-        if (!childrenAttr) childrenAttr = 'children';
-
-        var treeList = [];
-        var lookup = {};
-
-        list.forEach(function(obj) {
-            lookup[obj[idAttr]] = obj;
-            obj[childrenAttr] = [];
-        });
-        list.forEach(function(obj) {
-            if (obj[parentAttr] != null) {
-                lookup[obj[parentAttr]][childrenAttr].push(obj);
-            } else {
-                treeList.push(obj);
-            }
-        });
-        return treeList;
-    };
-
-    var newlist = treeify(list);
-    console.log(newlist);
-
-
-
-
-
-
-    /*function print(newlist) {
-        var newarr = [];
-        for (var i = 0; i < newlist.length; i++) {
-            if (newlist[i].parent === null){
-                newarr.push(newlist[i]);
-            }
-            document.getElementById('items').innerHTML = document.getElementById('items').innerHTML + '<li class="it">' + newlist[i].id + ' ' + newlist[i].title + '</li>';
-        }
-    }
-  /!*  function searchParent() {
-        for (var i = 0; i < newarr.length; i++) {
-            if (!)
-        }
-    }*!/
-
-    print(newlist);*/
-
-
-
-
-
-
-    /*function flattenAccounts(accounts){
-        var a = [];
-        for(var i=0;i<accounts.length;i++){
-            var o = accounts[i];
-            if(o.children){
-                var c = flattenAccounts(o.children);
-                if(c){
-                    a = a.concat(c);
+(function () {
+    const list = document.getElementById('posts-list');
+    let treeFragment = document.createDocumentFragment();
+    function getRequest(callback) {
+        let data;
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", "/structure-source", true);
+        xhr.onload = function (e) {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    data = JSON.parse(xhr.responseText);
+                    callback(data.tree);
+                } else {
+                    console.error(xhr.statusText);
                 }
             }
-            a.push(o)
-        }
-         console.log(a);
+        };
+        xhr.onerror = function (e) {
+            console.error(xhr.statusText);
+        };
+        xhr.send(null);
     }
-    flattenAccounts(newlist);*/
-
-
-
-
-
-
-
-
-
-/*    var printList = function(newlist){
-
-        for(var i=0;i< newlist.length;i++){
-
-            if(Array.isArray(newlist[i]))
-            {
-                printList(newlist[i]);
-            }
-            else{
-                document.write(newlist[i].id + " " + newlist[i].title +'<br/>');
-
-            }
-        }
+    getRequest((data) => {
+        data.sort((a, b) => {
+            return a.parent - b.parent;
+        });
+        let nestedArray = createNestedArray(data);
+        treeFragment.appendChild(nestedArray);
+        list.appendChild(treeFragment);
+        console.table(nestedArray)
+    });
+    function renderItem(item) {
+        let liElement = document.createElement('li');
+        liElement.classList.add( `post-${item.id}`);
+        liElement.innerText = item.title ;
+        return liElement;
     }
-
-    printList(newlist);
-    */
-
-
-
-
-
-
-    function printValues(obj) {
-        for (var key in obj) {
-            if (typeof obj[key] === 'object') {
-                printValues(obj[key]);
+    function createNestedArray(sortedArray) {
+        let resultUl = document.createElement('ul');
+        console.table(sortedArray)
+        sortedArray.forEach(item => {
+            if (!item.parent) {
+                resultUl.appendChild(renderItem(item));
+                return;
             }
-                else {
-                if (obj.hasOwnProperty('id') && obj.hasOwnProperty('title')) {
-                    document.getElementById('items').innerHTML = document.getElementById('items').innerHTML + '<li class="it">' + obj[key] + '</li>';
-                }
+            let parent = resultUl.querySelector(`.post-${item.parent}`)
+            if (!parent.children.length) {
+                let ul = document.createElement('ul');
+                parent.appendChild(ul);
             }
-        }
+            parent.children[0].appendChild(renderItem(item))
+        });
+        return resultUl;
     }
-    printValues(newlist);
-
-
-
-
-
-
-
-    /*for (var key in obj) {
-        document.getElementById('items').innerHTML = document.getElementById('items').innerHTML + '<li class="it">' + obj[key].id + " " + obj[key].title + '</li>';
-    }*/
-
-});
-
-
+    // function createNestedArray(sortedArray) {
+    //     let result = [];
+    //
+    //     sortedArray.forEach(item => {
+    //         if (!item.parent) {
+    //             result.push(item);
+    //             return;
+    //         }
+    //
+    //         let parent = findParent(result, item);
+    //
+    //         if (!parent.children) {
+    //             parent.children = [];
+    //         }
+    //
+    //         parent.children.push(item);
+    //
+    //     });
+    //
+    //     return result;
+    // }
+    //
+    // function findParent(resultArray, item) {
+    //     let result;
+    //
+    //     for (let i = 0, l = resultArray.length; i < l; i++) {
+    //         let parent = resultArray[i];
+    //
+    //         if (parent.id === item.parent) {
+    //             result = parent;
+    //             break;
+    //         }
+    //
+    //         if (parent.children) {
+    //            result = findParent(parent.children, item);
+    //
+    //            if (result) break;
+    //         }
+    //     }
+    //
+    //     return result;
+    // }
+})();
